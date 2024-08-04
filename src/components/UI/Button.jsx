28ebe712c5fb92c130938397f1ab304641abbc25
ofiralmagor <162960405+ofiralmagor@users.noreply.github.com>
2024-08-04 10:36:53 +0300

    Delete src directory

--- src/components/UI/Button.jsx
+++
@@ -1,9 +0,0 @@
-import './Button.css';
-
-export default function Button({ children, ...props }){
-      return (
-          <button className='flower-button' {...props}>
-              {children}
-          </button>
-      );
-  }
