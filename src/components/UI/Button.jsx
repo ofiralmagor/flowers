de6cefdb017e4e01fc7456ccfc63753c0091d9f6
ofiralmagor <162960405+ofiralmagor@users.noreply.github.com>
2024-08-04 10:37:25 +0300

    Add files via upload

---
+++ src/components/UI/Button.jsx
@@ -0,0 +1,9 @@
+import './Button.css';
+
+export default function Button({ children, ...props }){
+      return (
+          <button className='flower-button' {...props}>
+              {children}
+          </button>
+      );
+  }
---
+++ src/components/UI/Modal.jsx
@@ -0,0 +1,15 @@
+import './Modal.css';
+
+export default function Modal({ onClose, name, description, time }) {
+    return (
+        <div className="modal-overlay">
+        <div className="modal-container">
+          <h1>{name}</h1>
+          <p className='flower-item-description'>{description}</p> 
+          <h4 className='flower-item-h4'>:עונת הפריחה</h4>
+          <p className='flower-item-time'>{time}</p>
+          <button className="close-button" onClick={onClose}>Close</button>
+        </div>
+      </div>
+      );
+}
